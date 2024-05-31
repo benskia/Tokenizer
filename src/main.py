6eb5f5e3b0206f2e5194982ef6dc7f0b0c107a39
benskia <benski.abraham@gmail.com>
2024-05-31 17:02:29 -0500

    update main

--- src/main.py
+++ src/main.py
@@ -1,17 +1,61 @@
+import re
 from typing import Iterator
+
+
+class Trie:
+
+    def __init__(self) -> None:
+        self.root = {}
+        self.end_symbol = "\0"
+
+    def __repr__(self) -> str:
+
+        def list_words_r(trie: dict, end_symbol: str) -> list:
+            words = []
+            for k, v in trie.items():
+                if k != end_symbol:
+                    for ch in list_words_r(v, end_symbol):
+                        words.append(k + ch)
+                else:
+                    words.append(f" : {v}")
+            return words
+
+        output = ""
+        for word in list_words_r(self.root, self.end_symbol):
+            output += f"{word}\n"
+        return output
+
+    def add(self, word: str) -> None:
+        current = self.root
+        for letter in word:
+            if letter not in current:
+                current[letter] = {}
+                current = current[letter]
+        if self.end_symbol not in current:
+            current[self.end_symbol] = 1
+        else:
+            current[self.end_symbol] += 1
 
 
 def main() -> None:
     document: str = "src/input.txt"
     buffer_size: int = 4096
-    words: list[str] = []
-    [words.extend(chunk) for chunk in chunk_words(document, buffer_size)]
-    print(len(words))
-
-    # TODO: clean words
+    keywords: Trie = chunk_to_words(doc_to_chunk(document, buffer_size))
+    print(keywords)
 
 
-def chunk_words(filepath: str, buffer_size: int) -> Iterator[list[str]]:
+def remove_symbols_from_word(word: str) -> str:
+    return re.sub(r"[^\w]", "", word)
+
+
+def chunk_to_words(chunk: Iterator[list[str]]) -> Trie:
+    keywords = Trie()
+    for word in [word for words in chunk for word in words]:
+        keywords.add(remove_symbols_from_word(word))
+    return keywords
+
+
+def doc_to_chunk(filepath: str, buffer_size: int) -> Iterator[list[str]]:
     position: int = 0
     try:
         with open(filepath) as f:
